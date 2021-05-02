4be49f0f814e445fac55d120c92cb456e1baf56a
yashk1900 <58561261+yashk1900@users.noreply.github.com>
2021-05-02 20:05:04 +0530

    Removed a deployment bug

--- app.py
+++ app.py
@@ -57,4 +57,5 @@ def prediction(filename):
     # Step 5
     return render_template('predict.html', predictions=predictions)
 
-app.run(host='127.0.0.1', port=80)
+if __name__ == "__main__":
+    app.run(debug=True)
